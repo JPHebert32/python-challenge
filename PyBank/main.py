7cd1f8c824389bb09c7a402097cac64e1da3da02
JP Hebert <JPHebert32@gmail.com>
2020-03-31 00:11:37 -0600

    Changes to be committed: Calulated Totals for Months &P&L 	    modified:   main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,7 +18,7 @@
 # First we'll import the os module
 # This will allow us to create file paths across operating systems
 import os
-print(os.listdir('../../'))
+#print(os.listdir('../../'))
 
 
 # Module for reading CSV files
@@ -28,14 +28,26 @@ import csv
 csvpath = os.path.join('C:/Users/JPHeb/JPH-Data-Class/du-den-data-pt-03-2020-u-c/Homework-3-Python/PyBank/Resources/budget_data.csv')
 
 
+P_and_L = 0
+months = 0
 # Open the CSV
 with open(csvpath) as budget_csv:
     csvreader = csv.reader(budget_csv, delimiter=",")
 
     # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
-    print(f"{csv_header}")
+    #print(f"{csv_header}")
 
-    # Read each row of data after the header
+    # COUNT MONTHS AND SUM P&L
     for row in csvreader:
-        print(f'{row[0]},{row[1]}')
+        P_and_L = P_and_L + int(row[1])
+        months = months + 1
+
+print(F'Financial Analysis')
+print("----------------------------")
+print(f'Total Months: {months}')  
+print(f'TOTAL P&L: {P_and_L}')
+print(f'Average  Change: $') #$-2315.12
+print(f'Greatest Increase in Profits: ') #Feb-2012 ($1926159)
+print(f'Greatest Decrease in Profits: ') #Sep-2013 ($-2196167)
+
