3ce4c9aceb019ff7fe0d69d7aaabd1f962dee721
JP Hebert <JPHebert32@gmail.com>
2020-04-03 22:44:57 -0600

    Changes to be committed: # generate successive difference list 	modified:   main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,8 +13,8 @@ csvpath = os.path.join('C:/Users/JPHeb/JPH-Data-Class/du-den-data-pt-03-2020-u-c
 P_and_L = 0
 date = []
 revenue = []
-Gr_Increase = 0
-Gr_Decrease = 0
+Increase = 0
+Decrease = 0
 Average_Change = float(0)
 
 # Open the CSV
@@ -35,16 +35,36 @@ with open(csvpath) as budget_csv:
         # Add to Revenue List
         revenue.append(int(row[1]))
 
-total_months = len(date)
-total_revenue = sum(revenue)
+        
 
-         
+total_months = len(date)
+total = sum(revenue)
+
+Monthly_diff =[]
+
+# using zip() 
+# generate successive difference list 
+Monthly_diff = [j - i for i, j in zip(revenue[: -1], revenue[1 :])] 
+#print(Monthly_diff)
+
+#Find Greatest Increase & Greateast Decrease Using MAX and Min on Monthly_Diff
+#print(max(Monthly_diff))
+#print(min(Monthly_diff))
+
+
+Total_Diff = sum(Monthly_diff)
+diff_count = len(Monthly_diff)
+Average_Change = Total_Diff / diff_count
+
+
+
+
 
 print(f'Financial Analysis')
 print("----------------------------")
-print(f'Total Months: {total_months}')  
-print(f'TOTAL P&L: {total_revenue}')
-print(f'Average  Change: $') #$-2315.12
-print(f'Greatest Increase in Profits: ') #Feb-2012 ($1926159)
-print(f'Greatest Decrease in Profits: ') #Sep-2013 ($-2196167)
+print(f'Total Months: {total_months}')  #86
+print(f'TOTAL P&L: {total}') # $
+print(f'Average  Change: ${Average_Change}') #$-2315.12
+print(f'Greatest Increase in Profits: **DATE**  ${max(Monthly_diff)}') #Feb-2012 ($1926159)
+print(f'Greatest Decrease in Profits: **DATE**  ${min(Monthly_diff)}') #Sep-2013 ($-2196167)
 
